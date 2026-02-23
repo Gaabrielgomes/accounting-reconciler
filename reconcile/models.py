aa6e4f4cc4da96708b52e38458c22dee71ed9b10
Gaabrielgomes <gaabrielgomes3241@gmail.com>
2026-02-23 12:00:49 -0300

    First models configured

--- reconcile/models.py
+++ reconcile/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    id = models.AutoField(serialize=True, primary_key=True)
+    associated_company = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    cpf_cnpj = models.IntegerField(null=True, blank=True)
+
+
+class Register(models.Model):
+    class RegisterType(models.TextChoices):
+        INBOUND = "IN"
+        OUTBOUND = "OUT"  
+
+    id = models.AutoField(serialize=True, primary_key=True)
+    company_id = models.IntegerField()
+    issue_date = models.DateField()
+    text = models.TextField(max_length=100)
+    register_type = models.TextField(
+        max_length=3, 
+        choices=RegisterType, 
+        default=RegisterType.INBOUND
+    )
+
