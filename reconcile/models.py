8a5863579aabd54a0b1a7e497ba913a6b768c1f5
GaabrielGomes <gaabrielgomes3241@gmail.com>
2026-02-23 16:09:33 -0300

    Updated classes from models

--- reconcile/models.py
+++ reconcile/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 
+
+class Client(models.Model):
+    
+    id = models.AutoField(serialize=True, primary_key=True)
+    name = models.CharField(max_length=100)
+    register_date = models.DateField()
+    cpf_cnpj = models.IntegerField(null=True, blank=True)
+
+
 # Create your models here.
 class Supplier(models.Model):
+    
     id = models.AutoField(serialize=True, primary_key=True)
-    associated_company = models.CharField(max_length=100)
+    associated_company = models.IntegerField()
     name = models.CharField(max_length=100)
     cpf_cnpj = models.IntegerField(null=True, blank=True)
 
 
 class Register(models.Model):
+    
     class RegisterType(models.TextChoices):
-        INBOUND = "IN"
-        OUTBOUND = "OUT"  
+        INBOUND = "INBOUND"
+        OUTBOUND = "OUTBOUND"
 
     id = models.AutoField(serialize=True, primary_key=True)
     company_id = models.IntegerField()
     issue_date = models.DateField()
-    text = models.TextField(max_length=100)
+    debit_account = models.CharField(max_length=100, default="")
+    credit_account = models.CharField(max_length=100, default="")
+    description = models.TextField(max_length=100)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
     register_type = models.TextField(
-        max_length=3, 
-        choices=RegisterType, 
-        default=RegisterType.INBOUND
+        choices=RegisterType,
+        null=True
     )
-
