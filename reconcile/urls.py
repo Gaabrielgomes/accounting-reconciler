3ebc6683bf18a481597f6bc18380204b1d3bd801
GaabrielGomes <gaabrielgomes3241@gmail.com>
2026-02-23 17:58:44 -0300

    Updated manual-add route and added text-add route

--- reconcile/urls.py
+++ reconcile/urls.py
@@ -6,5 +6,6 @@ app_name = "reconcile"
 
 urlpatterns = [
     path('', views.reconcile_home),
-    path('add/', views.reconcile_add, name="add")
+    path('manual-add/', views.reconcile_manual_add, name="manual_add"),
+    path('text-add/', views.reconcile_text_add, name="text_add")
 ]
