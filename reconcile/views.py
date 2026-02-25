e283e526fcdfff30cdd44c36ddd6eec613ac5948
GaabrielGomes <gaabrielgomes3241@gmail.com>
2026-02-25 10:28:48 -0300

    Added 'process_file' view

--- reconcile/views.py
+++ reconcile/views.py
@@ -3,7 +3,19 @@ from .forms import ReconcileForm
 
 # Create your views here.
 def reconcile_home(request):
-    variables = {
-        "form": ReconcileForm()
-    }
-    return render(request, 'pages/reconcile_home.html', variables)
+    return render(request, 'pages/reconcile_home.html', {'form': ReconcileForm()})
+
+
+def process_file(request):
+    if request.method == 'POST':
+        form = ReconcileForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            uploaded_file = form.cleaned_data['file']
+            return render(
+                request,
+                  'pages/process_file.html',
+                    {'file_name': uploaded_file.name}
+            )
+    else:
+        return render(request, 'pages/reconcile_home.html', {'form': ReconcileForm()})
