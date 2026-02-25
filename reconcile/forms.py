ca05be2818f521c5f0dd06b42c1a3115a1bc079b
GaabrielGomes <gaabrielgomes3241@gmail.com>
2026-02-25 09:37:40 -0300

    Manual and multi clients adding methods removed due to current disuse

--- reconcile/forms.py
+++ reconcile/forms.py
@@ -2,17 +2,7 @@ from django import forms
 
 
 class ReconcileForm(forms.Form):
-    file = forms.FileField(label='Select a file')
-
-class ManualAddForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=100)
-    document = forms.FileField(label='Select a file')
-
-class TextAddForm(forms.Form):
-    file = forms.FileField(
-        label="Selecione um arquivo de texto",
-        widget=forms.ClearableFileInput(attrs={
-            "class": "form-input",
-            "accept": ".txt"
-        })
-    )
+    file = forms.FileField(label='Select a file (.xlsx, .csv, .json, .txt)', widget=forms.ClearableFileInput(attrs={
+        "class": "form-input",
+        "accept": ".csv, .txt, .xlsx, .json"
+    }))
--- reconcile/views.py
+++ reconcile/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import ReconcileForm, ManualAddForm, TextAddForm
+from .forms import ReconcileForm
 
 # Create your views here.
 def reconcile_home(request):
@@ -7,33 +7,3 @@ def reconcile_home(request):
         "form": ReconcileForm()
     }
     return render(request, 'pages/reconcile_home.html', variables)
-
-
-def reconcile_manual_add(request):
-    form = ManualAddForm()
-
-    if request.method == "POST":
-        form = ManualAddForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data["name"]
-            document = form.cleaned_data["document"]
-
-            print(name, document)  # depois você salva no banco
-
-    return render(request, "pages/reconcile_manual_add.html", {"form": form})
-
-
-def reconcile_text_add(request):
-    if request.method == "POST":
-        form = TextAddForm(request.POST, request.FILES)
-        if form.is_valid():
-            uploaded_file = form.cleaned_data["file"]
-
-            # Exemplo: ler conteúdo do arquivo
-            content = uploaded_file.read().decode("utf-8")
-            print(content)
-
-    else:
-        form = TextAddForm()
-
-    return render(request, "pages/reconcile_text_add.html", {"form": form})
